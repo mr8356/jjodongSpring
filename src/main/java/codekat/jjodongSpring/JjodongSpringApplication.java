386a8cca8f7c8519a5c378602008b42f5e82d992
KonkukCodeCat <mr8356@naver.com>
2022-05-18 00:42:25 +0900

    start

---
+++ src/main/java/codekat/jjodongSpring/JjodongSpringApplication.java
@@ -0,0 +1,13 @@
+package codekat.jjodongSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjodongSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjodongSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codekat/jjodongSpring/JjodongSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package codekat.jjodongSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjodongSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
