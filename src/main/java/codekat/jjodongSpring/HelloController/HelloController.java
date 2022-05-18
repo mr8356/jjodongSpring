925feb76f86d9fa37d400ec51f3ab4473920cd43
mr8356 <mr8356@naver.com>
2022-05-18 06:14:27 +0000

    api 만들어봄!

--- src/main/java/codekat/jjodongSpring/HelloController/HelloController.java
+++ src/main/java/codekat/jjodongSpring/HelloController/HelloController.java
@@ -2,7 +2,10 @@ package codekat.jjodongSpring.HelloController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController{
@@ -11,5 +14,33 @@ public class HelloController{
     public String hello(Model model) {
         model.addAttribute("data" , "hello!!");
         return "hello";
+    }
+    
+    @GetMapping("hello-jjodong")
+    public String helloJjodong(Model model){
+        model.addAttribute("name"  , "조동현");
+        return "hello-jjodong";
+    }
+    
+    @GetMapping("helloApi")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    
+    public class Hello{
+        private String name;
+        
+        public String getName() {
+            return name;
+        }
+        
+        public String setName(String name){
+            this.name = name;
+            return this.name;
+        }
+        
     }
 }
