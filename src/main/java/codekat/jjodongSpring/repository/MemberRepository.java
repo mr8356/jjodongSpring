7a66311258841c6ca60f266b02f8fb1230703a97
KonkukCodeCat <mr8356@naver.com>
2022-05-20 13:47:43 +0900

    create domain , Member, repository

---
+++ src/main/java/codekat/jjodongSpring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package codekat.jjodongSpring.repository;
+
+import codekat.jjodongSpring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
